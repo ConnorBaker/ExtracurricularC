a260dfc513792aac6548fd96c3b4a2b46e83bf85
Connor Baker <connorbaker01@gmail.com>
2017-11-14 14:29:28 -0500

    ChangeOfBase: Version 0.1b

--- ChangeOfBase/ChangeOfBase.c
+++ ChangeOfBase/ChangeOfBase.c
@@ -1,24 +1,34 @@
 // Author: Connor Baker
 // Created: November 14, 2017
-// Version: 0.1a
+// Version: 0.1b
 
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <gmp.h>
 
-long double numberToConvert;
-long double desiredBase;
-long accuracy;
+#define DECIMAL_ACCURACY 8192 // Todo: Set this value at runtime
+
+mpf_t numberToConvert;
+mpf_t desiredBase;
+int accuracy;
+//int DECIMAL_ACCURACY;
+char *heap;
 
 
 void convertBaseToBase(char *heap) {
-	char floorOfConversion;
-	long i;
-	for (i = 2; i < accuracy + 2; i++) {
-		numberToConvert *= desiredBase;
-		floorOfConversion = (char) numberToConvert;
-		heap[i] = (char) (floorOfConversion + '0');
-		numberToConvert -= floorOfConversion;
+	char wholePartOfProduct;
+	mpf_t currentNumber;
+	mpf_init2(currentNumber, DECIMAL_ACCURACY);
+	mpf_set(currentNumber, numberToConvert);
+
+	for (int i = 2; i < accuracy + 2; i++) {
+		mpf_mul(currentNumber, currentNumber, desiredBase);
+		wholePartOfProduct = (char) mpf_get_d(currentNumber);
+//		gmp_printf("i = %d \t current num is: %.Ff\n", i, currentNumber);
+//		gmp_printf("\t whole part is: %d\n", wholePartOfProduct);
+		heap[i] = (char) (wholePartOfProduct + '0');
+		mpf_sub_ui(currentNumber, currentNumber, (unsigned long) wholePartOfProduct);
 	}
 }
 
@@ -30,22 +40,26 @@ int main(int argc, char *argv[]) {
 		return 0;
 	}
 
+	heap = (char *) calloc((size_t) (accuracy + 3L), sizeof(char));
+
 	// Set values and print them to establish that the program is working as expected
-	numberToConvert = strtold((const char *) argv[1], NULL);
-	printf("numberToConvert: %Lf\n", numberToConvert);
-	desiredBase = strtold((const char *) argv[2], NULL);
-	printf("desiredBase: %Le\n", desiredBase);
-	accuracy = strtol((const char *) argv[3], NULL, 10);
-	printf("numberToConvert: %li\n", accuracy);
+	mpf_init2(numberToConvert, DECIMAL_ACCURACY);
+	mpf_set_str(numberToConvert, argv[1], 10);
+	gmp_printf("numberToConvert: %.Ff\n", numberToConvert);
+	mpf_init2(desiredBase, DECIMAL_ACCURACY);
+	mpf_set_str(desiredBase, argv[2], 10);
+	gmp_printf("desiredBase: %.Ff\n", desiredBase);
+	accuracy = (int) strtol((const char *) argv[3], NULL, 10);
+	printf("accuracy: %d\n", accuracy);
 
-	char newRepresentation[accuracy+2];
-	newRepresentation[0] = '0';
-	newRepresentation[1] = '.';
-	convertBaseToBase(newRepresentation);
+//	DECIMAL_ACCURACY = accuracy * 4;
 
-	printf("conversion is %s\n", newRepresentation);
+	heap[0] = '0';
+	heap[1] = '.';
+	heap[accuracy+2] = '\0'; // No more trailing garbage
+	convertBaseToBase(heap);
 
-
+	printf("conversion is %s\n", heap);
 
 	return 0;
 }
